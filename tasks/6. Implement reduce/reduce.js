f6ed281b7fa0cb39144817d1e69365d2a8252a8e
Shafin-A <30351904+Shafin-A@users.noreply.github.com>
2021-08-24 13:39:48 -0400

    reduce task

--- tasks/6. Implement reduce/reduce.js
+++ tasks/6. Implement reduce/reduce.js
@@ -1,3 +1,11 @@
 export function Reduce(array, reducer, initialValue) {
-  // TODO: write your code here
+    if (array.length === 0) { // base case
+        return initialValue;
+    }
+    else {
+        const [head, ...rest] = array; // split array
+        const acc = reducer(initialValue, head); // apply reducer
+
+        return Reduce(rest, reducer, acc); // recurse
+    }
 }
