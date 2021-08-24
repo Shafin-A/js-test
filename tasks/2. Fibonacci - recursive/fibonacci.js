77eafb346cd13bc0934f3c86cbcd10fddf130ce6
Shafin-A <30351904+Shafin-A@users.noreply.github.com>
2021-08-24 12:55:07 -0400

    fibonacci task

--- tasks/2. Fibonacci - recursive/fibonacci.js
+++ tasks/2. Fibonacci - recursive/fibonacci.js
@@ -1,3 +1,21 @@
 export function getFibonacciUntil(n) {
-  //TODO write your code here, remember it should be recursive
+    /*
+    fib(5) - arr = [0, 1, 1, 2, 3], push(arr[4]: 3 + arr[3]: 2, return [0, 1, 1, 2, 3, 5]
+        fib(4) - arr = [0, 1, 1, 2], push(arr[3]: 2 + arr[2]: 1, return arr = [0, 1, 1, 2, 3]
+            fib(3) - arr = [0, 1, 1], push(arr[2]: 1 + arr[1]: 1, return arr = [0, 1, 1, 2]
+                fib(2) - arr = [0,1], push(arr[1]: 1 + arr[0]: 0, return arr = [0, 1, 1]
+                    fib(1) - return [0, 1]
+     */
+
+    const fib = n => {
+        if (n === 0) return [0];
+        if (n === 1) return [0, 1];
+
+        const arr = fib(n - 1);
+        arr.push(arr[n - 1] + arr[n - 2]);
+
+        return arr;
+    }
+
+    return fib(n-1);
 }
