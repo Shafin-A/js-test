8847b57f25f3b43a88488e6e94de50d9b0b848eb
Shafin-A <30351904+Shafin-A@users.noreply.github.com>
2021-08-24 13:18:38 -0400

    fizzbuzz task

--- tasks/4. FizzBuzz/fizzbuzz.js
+++ tasks/4. FizzBuzz/fizzbuzz.js
@@ -1,3 +1,21 @@
 export function getFizzBuzzUntil(n) {
-  // TODO: write your code here
+    // FizzBuzz
+    let arr = [];
+
+    for (let i = 0; i < n; i++) {
+        if (( (i + 1) % 3 === 0 ) && ( (i + 1) % 5 === 0 )) {
+            arr.push("FizzBuzz");
+        }
+        else if ( (i + 1) % 3 === 0 ){
+            arr.push("Fizz");
+        }
+        else if ( (i + 1) % 5 === 0 ) {
+            arr.push("Buzz");
+        }
+        else {
+            arr.push(i+1)
+        }
+    }
+
+    return arr;
 }
