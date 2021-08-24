cdcff0a3636ffc7099958fa23e235b2055527748
Shafin-A <30351904+Shafin-A@users.noreply.github.com>
2021-08-24 13:10:40 -0400

    balanced parentheses task

--- tasks/3. Balanced parentheses/balancedParentheses.js
+++ tasks/3. Balanced parentheses/balancedParentheses.js
@@ -1,3 +1,32 @@
 export function areParenthesesBalanced(inputString) {
-  // TODO: write your code here
+
+    // build new string with only parentheses
+    const getOnlyParentheses = inputString => {
+        let onlyParString = "";
+
+        for (let i = 0; i < inputString.length; i++) {
+            let curChar = inputString[i];
+            if (curChar === '(' || curChar === ')') onlyParString += curChar;
+        }
+
+        return onlyParString;
+    }
+
+    const balancedParentheses = inputString => {
+        let stack = []; // use a stack to check if parentheses are balanced
+
+        for (let i = 0; i < inputString.length; i++) {
+            let curChar = inputString[i];
+
+            if (curChar === '(') stack.push(')'); // push the opposite parenthesis
+            else if (stack.length === 0 || stack.pop() !== curChar) { // if ')' pop stack and check if they match
+                return false;
+            }
+        }
+
+        return stack.length === 0; // balanced if empty
+    }
+
+    const onlyParString = getOnlyParentheses(inputString);
+    return balancedParentheses(onlyParString);
 }
