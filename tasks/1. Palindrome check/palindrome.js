950dc405966e9efafc343364d3ee279eff152f01
Shafin-A <30351904+Shafin-A@users.noreply.github.com>
2021-08-24 11:45:26 -0400

    palindrome task

--- tasks/1. Palindrome check/palindrome.js
+++ tasks/1. Palindrome check/palindrome.js
@@ -1,3 +1,12 @@
 export function isPalindrome(inputString) {
-  // TODO: write your code here
+    // goes through string with two pointers at start and end, and compares characters
+    // assumes isPalindrome() is case sensitive, i.e. Racecar is not a palindrome but racecar or RacecaR is
+    // if case insensitive, we would use .toLowerCase()
+    for (let i = 0; i < inputString.length / 2; i++) {
+        let j = inputString.length - i - 1;
+        if (inputString[i] !== inputString[j]) {
+            return false;
+        }
+    }
+    return true;
 }
