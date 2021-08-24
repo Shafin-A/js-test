39b92466d6d4b69dabcb48df466740e1b590430c
Shafin-A <30351904+Shafin-A@users.noreply.github.com>
2021-08-24 13:30:25 -0400

    caesar shift task

--- tasks/5. Caesar cipher/caesar.js
+++ tasks/5. Caesar cipher/caesar.js
@@ -1,3 +1,25 @@
 export function encryptCaesar(inputString, key) {
-  // TODO: write your code here
+    if (key < 0) { // negative key wrap
+        return encryptCaesar(inputString, key + 26);
+    }
+
+    let result = "";
+
+    for (let i = 0; i < inputString.length; i++) {
+        let curCharCode = inputString.charCodeAt(i);
+        let shiftedChar = "";
+
+        // uppercase letters
+        if (curCharCode >= 65 && curCharCode <= 90) {
+            shiftedChar = String.fromCharCode(((curCharCode - 65 + key) % 26) + 65); // handles wrapping with %
+        }
+        // lowercase letters
+        else if (curCharCode >= 97 && curCharCode <= 122) {
+            shiftedChar = String.fromCharCode(((curCharCode - 97 + key) % 26) + 97);
+        }
+
+        result += shiftedChar;
+    }
+
+    return result;
 }
